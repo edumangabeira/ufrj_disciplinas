76e5df4d0fdaeeb8fca2951f03108d2b0d82f08e
Eduardo <edu.mangaba@gmail.com>
2018-09-11 09:54:13 -0300

    update lista1

---
+++ listas/lista_1/matematica.c
@@ -0,0 +1,43 @@
+/* 
+Implementa o conjunto de funcoes para operacoes matematicas basicas declaradas em matematica.h
+*/
+
+/* 
+Descricao: soma dois numeros inteiros
+Entrada: os dois numeros inteiros
+Saida: a soma dos dois numeros
+Restricoes:
+*/
+int soma (int x, int y) {
+   return x + y;
+}
+
+/* 
+Descricao: subtrai dois numeros inteiros
+Entrada: os dois numeros (minuendo e subtraendo)
+Saida: o resto da subtracao
+Restricoes:
+*/
+int subtrai (int x, int y) {
+   return x - y;
+}
+
+/* 
+Descricao: divide dois numeros inteiros
+Entrada: os dois numeros (dividendo e divisor)
+Saida: o quociente da divisao
+Restricoes: o divisor deve ser diferente de zero
+*/
+int divide (int x, int y) {
+   return x / y;
+}
+
+/* 
+Descricao: multiplica dois numeros inteiros
+Entrada: os dois numeros inteiros
+Saida: o produto dos dois numeros
+Restricoes:
+*/
+int multiplica (int x, int y) {
+   return x * y;
+}
---
+++ listas/lista_1/matematica.h
@@ -0,0 +1,35 @@
+/* 
+Declara um conjunto de funcoes para operacoes matematicas basicas 
+*/
+
+/* 
+Descricao: soma dois numeros inteiros
+Entrada: os dois numeros inteiros
+Saida: a soma dos dois numeros
+Restricoes:
+*/
+int soma (int x, int y);
+
+/* 
+Descricao: subtrai dois numeros inteiros
+Entrada: os dois numeros (minuendo e subtraendo)
+Saida: o resto da subtracao
+Restricoes:
+*/
+int subtrai (int x, int y);
+
+/* 
+Descricao: divide dois numeros inteiros
+Entrada: os dois numeros (dividendo e divisor)
+Saida: o quociente da divisao
+Restricoes: o divisor deve ser diferente de zero
+*/
+int divide (int x, int y);
+
+/* 
+Descricao: multiplica dois numeros inteiros
+Entrada: os dois numeros inteiros
+Saida: o produto dos dois numeros
+Restricoes:
+*/
+int multiplica (int x, int y);
