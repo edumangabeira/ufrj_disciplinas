e98d259bef53413f1c0e9aecb469af4dc6476534
eduardo <edu.mangaba@gmail.com>
2018-09-18 18:21:03 +0000

    (quickfix)lab4

--- lab4/ed_operations.c
+++ lab4/ed_operations.c
@@ -2,7 +2,6 @@
 /*
 Implementa conjunto de operacoes para os exercicios propostos em sala.
 */
-
 //Compara o tempo entre duas realizacoes de uma mesma tarefa.
 int compara_tempo(Tempo t1, Tempo t2){
 	int r;
