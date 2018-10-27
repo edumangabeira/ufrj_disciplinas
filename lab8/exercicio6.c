459216050429f849252dfda78f7171538e153d64
eduardo <edu.mangaba@gmail.com>
2018-10-27 14:06:51 +0000

    (wip)lab 8

---
+++ lab8/exercicio6.c
@@ -0,0 +1,21 @@
+/*
+Eduardo Freire Mangabeira
+Data:26/10/2018
+github: /edumangabeira
+descrição: sexta atividade do lab 8
+
+Entrada: tres numeros reais
+Saída: media aritmetica dos dois reais maiores
+*/
+
+#include<stdio.h>
+
+int main(void){
+	float num1, num2, num3, media;
+	printf("\n\tdigite os numeros reais separados por espacos\n\n");
+	scanf("%f%f%f",&num1, &num2, &num3);
+
+	media = (num1<num2) && (num1<num3) ? (num2 + num3)/2 : (num2 > num3) ? (num2+num1)/2 : (num1+num3)/2 ;
+	printf("a media eh: %3.f\n", media);
+	return 0;
+}
