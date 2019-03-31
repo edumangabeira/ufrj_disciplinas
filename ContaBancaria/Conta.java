59cef045b31691ca4fc8d5eaf854d4158ebbf3b0
edumangabeira <edu.mangaba@gmail.com>
2019-03-31 00:06:41 -0300

    como posso salvar instâncias diferentes num 'banco'?

--- ContaBancaria/Conta.java
+++ ContaBancaria/Conta.java
@@ -21,7 +21,7 @@ public class Conta{
 	}
 
 	// saca	uma	quantidade x
-	public static void saque(float valor){
+	void saque(float valor){
 		if(valor < this.saldo){
 			this.saldo = this.saldo - valor;
 			System.out.println("Operação autorizada pela instituição");
@@ -30,39 +30,40 @@ public class Conta{
 	}
 
 	// deposita uma quantidade x
-	public static void deposito(float valor){
+	void deposito(float valor){
 		this.saldo = this.saldo + valor;
 		System.out.println("Depositado com sucesso - Valor "+valor
 						  + "R$");
 	}
 
-	
-	// como usar lambda???(precisa?)
-	/*
-	Saldo -> this.saldo
-	
-	// devolve o saldo atual
-	public static int saldo(){
-		return this.saldo
-
-	}
-	*/
 
 	// transfere uma quantidade x para uma outra conta y
-	public static void transfere(float valor, Conta conta){
+	void transfere(float valor, Conta conta){
 		this.saldo = this.saldo - valor;
 		conta.saldo = conta.saldo + valor;
 		System.in.println("Operação realizada com sucesso");
 	}
 
 	// imprime o nome do titular da conta
-	public static void nomeTitular(){
+	void nomeTitular(){
+
 		System.in.println(this.cliente);
 	}
 
 	// devolve o tipo de conta
-	public static void tipoConta(){
+	void tipoConta(){
 
 	}
+
+	// como usar lambda???(precisa?)
+	/*
+	Saldo -> this.saldo
+
+	// devolve o saldo atual
+	int saldo(){
+		return this.saldo
+
+	}
+	*/
 
 }
--- ContaBancaria/ContaBancaria.java
+++ ContaBancaria/ContaBancaria.java
@@ -1,6 +1,32 @@
 public class ContaBancaria{
 	
 	public static void main(String[] args){
+
+		boolean stop;
+		Conta conta;
+		// fazer vetor de ponteiros apontando pra cada instância da classe conta
+		conta = new Conta();
+		for (int i=0; i<n; i++){
+			switch(conta[i]){
+				case 1:
+					System.out.println("Conta corrente");
+					break;
+
+				case 2:
+					System.out.println("Conta poupança");
+					break;
+
+				case 3:
+					System.out.println("conta salário");
+					break;
+				default:
+					System.out.printf("Conta inválida");
+					stop = true;
+					break;
+			}
+			if(!stop)
+				conta.userData(numero, saldo, cliente, limite);
+		}
 		
 	}
 }
